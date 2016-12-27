4cd57bf3f4e8315bd095a83d30f90aed34b7ec64
APP技術部-李慧茹 <lala.lee@mitake.com.tw>
2016-12-27 14:24:18 +0800

    修改TicketSchema增加Rating欄位

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 var TicketSchema = new mongoose.Schema({
-  name: String, 
-  imgurl: String,
-  price: Number,
-  currency: String,
-  startdate: String,
-  enddate: String
+    name: String,
+    imgurl: String,
+    price: Number,
+    currency: String,
+    startdate: String,
+    enddate: String,
+    rating: Number
 });
 module.exports = mongoose.model('Ticket', TicketSchema);
